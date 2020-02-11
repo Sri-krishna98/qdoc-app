2cc08174f055ee2790d3e4d52793f73dcc4185e9
Sri-krishna98 <45160214+Sri-krishna98@users.noreply.github.com>
2020-02-11 16:20:45 +0530

    add src

---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import logo from './Qdocs.png';
+// class Welcome extends Component {
+//     render() {
+//         return <img src={logo} alt="Logo"></img>
+//     }
+// }
+
+export const Welcome = () => <img src={logo} alt="Logo"></img>
