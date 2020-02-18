0c906cdfe6c14b20bb21bf221f48f5066b4e81ff
Sri-krishna98 <45160214+Sri-krishna98@users.noreply.github.com>
2020-02-18 17:10:36 +0530

    commit

--- src/components/Greet.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-
-
-export const Greet = () => <h1>Hello Qdocs</h1>
--- src/components/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import logo from './Qdocs.png';
-// class Welcome extends Component {
-//     render() {
-//         return <img src={logo} alt="Logo"></img>
-//     }
-// }
-
-export const Welcome = () => <img src={logo} alt="Logo"></img>
